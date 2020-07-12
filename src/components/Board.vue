db64b44aaaee89d28f2725f6029fce1543c3903c
leandroramirez <leandroramirez@gmail.com>
2020-07-12 20:57:50 -0300

    Initial commit

---
+++ src/components/Board.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>Nada</div>
+    <div>vertical</div>
+    <div>ho2rizontal</div>
+    <Grid/>
+  </div>
+</template>
+
+<script>
+import Grid from "@/components/Grid";
+
+export default {
+  components: {
+    Grid
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nng-grid">
+    <div v-for="value of pixelmap.pixels">{{ value }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      pixelmap: {
+        width: 3,
+        height: 2,
+        colors: {
+          "0": "#ffffff", // The first item is the background color
+          "1": "#000000"
+        },
+        pixels: Array.from("010101")
+      }
+    };
+  },
+  computed: {}
+};
+</script>
+
+<style scoped>
+.nng-grid {
+}
+</style>
